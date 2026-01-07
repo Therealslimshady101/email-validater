d7eb57b87c62922e5a89183614e00faae2420d9b
itsharry_ <franceh405@gmail.com>
2026-01-07 15:44:37 +0000

    Add files via upload

---
+++ email validater.py
@@ -0,0 +1,14 @@
+email = input("Enter an email address")
+valid = false
+
+for x = 10
+    if email[x] = "@" then
+        valid = true
+    end if
+next x
+
+if valid = false then
+PRINT("Email invalid")
+else
+    print("email valid")
+endif
